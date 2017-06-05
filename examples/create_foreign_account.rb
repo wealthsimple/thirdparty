5a48b2a18d257bdbe0bbc9f83261c46c7e639715
Peter Graham <peterghm@gmail.com>
2017-06-05 18:08:54 -0400

    remove irrelevant comments

--- examples/create_foreign_account.rb
+++ examples/create_foreign_account.rb
@@ -71,71 +71,7 @@ account = Thirdparty::Account.create(account_id, {
 
 pp account
 
-# #<Thirdparty::Account:0x007fb42713d868
-#  @response_json=
-#   {"account_id"=>"fa1c6f7b-07ed-429d-a9a2-257e97658d58",
-#    "status"=>"pending",
-#    "customer_type"=>"individual",
-#    "options"=>false,
-#    "applicants"=>
-#     [{"applicant_id"=>"bc182a29-9098-4af8-9e2f-5ecf4fef3788",
-#       "name"=>{"first"=>"Test", "middle"=>"Demo", "last"=>"User"},
-#       "email"=>"abcd1234@test.com",
-#       "birthday"=>"1990-12-22",
-#       "ssn"=>"***-**-6789",
-#       "citizenship_country"=>"USA",
-#       "mobile"=>"123456789",
-#       "address"=>
-#        {"line_1"=>"123 Fake Street",
-#         "city"=>"Test",
-#         "state"=>"Test",
-#         "postal_code"=>"12345",
-#         "country"=>"USA"},
-#       "employment_status"=>"employed"}],
-#    "notes"=>
-#     [{"note"=>"Status changed to Pending",
-#       "timestamp"=>"2017-06-05T21:02:50+00:00"},
-#      {"note"=>"Account information submitted",
-#       "timestamp"=>"2017-06-05T21:02:50+00:00"}],
-#    "can_fund"=>false,
-#    "can_trade"=>false,
-#    "can_trade_options"=>false},
-
 # Wait for account approval (usually takes a few seconds)
 sleep 5
 
 pp Thirdparty::Account.find(account_id)
-
-# #<Thirdparty::Account:0x007ff3a509cf90
-#  @response_json=
-#   {"account_id"=>"fa1c6f7b-07ed-429d-a9a2-257e97658d58",
-#    "status"=>"approved",
-#    "customer_type"=>"individual",
-#    "options"=>false,
-#    "applicants"=>
-#     [{"applicant_id"=>"bc182a29-9098-4af8-9e2f-5ecf4fef3788",
-#       "name"=>{"first"=>"Test", "middle"=>"Demo", "last"=>"User"},
-#       "email"=>"abcd1234@test.com",
-#       "birthday"=>"1990-12-22",
-#       "ssn"=>"***-**-6789",
-#       "citizenship_country"=>"USA",
-#       "mobile"=>"123456789",
-#       "address"=>
-#        {"line_1"=>"123 Fake Street",
-#         "city"=>"Test",
-#         "state"=>"Test",
-#         "postal_code"=>"12345",
-#         "country"=>"USA"},
-#       "employment_status"=>"employed"}],
-#    "notes"=>
-#     [{"note"=>"Identity check passed",
-#       "timestamp"=>"2017-06-05T21:02:52+00:00"},
-#      {"note"=>"Status changed to Approved",
-#       "timestamp"=>"2017-06-05T21:02:52+00:00"},
-#      {"note"=>"Status changed to Pending",
-#       "timestamp"=>"2017-06-05T21:02:50+00:00"},
-#      {"note"=>"Account information submitted",
-#       "timestamp"=>"2017-06-05T21:02:50+00:00"}],
-#    "can_fund"=>true,
-#    "can_trade"=>true,
-#    "can_trade_options"=>false},
